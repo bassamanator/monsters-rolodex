7b268d4bd1746a824b0899939dc1a603ce2e6fd6
bassamanator <bassam.husain@gmail.com>
2020-10-19 16:57:45 -0400

    Updates

---
+++ precache-manifest.12f6ba9069a00afddcbd617be6fe6f1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a15b2162c8b5caec4241c0243a61c8d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b623c695eb1f0181acd3",
+    "url": "/monsters-rolodex/static/css/main.edf7e699.chunk.css"
+  },
+  {
+    "revision": "000e21a15ac4f74a604a",
+    "url": "/monsters-rolodex/static/js/2.cbb7fb3b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.cbb7fb3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b623c695eb1f0181acd3",
+    "url": "/monsters-rolodex/static/js/main.d3d21008.chunk.js"
+  },
+  {
+    "revision": "e01b263a933f37eb7c2d",
+    "url": "/monsters-rolodex/static/js/runtime-main.6136c16c.js"
+  }
+]);
